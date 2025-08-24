62ec630a6dc7f89b3afae317432c02b8e2c7c03b
jmhw826 <168543143+jmhw826@users.noreply.github.com>
2025-08-24 15:59:56 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,6 +3,22 @@ const CUSTOMER_SITES = {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
     }
+    iqiyi: {
+        api: 'https://www.iqiyizyapi.com/api.php/provide/vod',
+        name: 'iqiyi资源',
+    }
+    xibaocaiji: {
+        api: 'https://www.xxibaozyw.com/api.php/provide/vod',
+        name: '细胞采集黄色',
+    }
+    yingshigongchang: {
+        api: 'https://cj.lziapi.com/api.php/provide/vod/',
+        name: '影视工厂',
+    }
+    fantuanyingshi: {
+        api: 'https://www.fantuan.tv/api.php/provide/vod/',
+        name: '饭团影视',
+    }    
 };
 
 // 调用全局方法合并
